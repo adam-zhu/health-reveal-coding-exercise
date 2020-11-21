0543f9fff1b2aa473685319dba1da9e96132d7f4
Adam Zhu <adam@adamz.hu>
2020-11-21 16:41:49 -0500

    fix missing imports, update vercel config with domain

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import '../styles/App.css';
 import { useFetchUser } from '../lib/user';
+import Login from '../components/Auth/Login';
+import Logout from '../components/Auth/Logout';
 
 export default function MyApp({ Component, pageProps }) {
   const { user, loading } = useFetchUser();
